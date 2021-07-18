c889a802580fbafac8c03c7af48f4aee7fe239a6
Daxxon Vong <Daxxonvong@yahoo.co.nz>
2021-07-18 16:16:10 +1200

    Add functionality to populate profile experience using PUT. Installed prettier.js, to format json.

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -156,13 +156,74 @@ router.delete('/', auth, async (req, res) => {
         // Remove user
         await User.findOneAndRemove({ _id: req.user.id });
 
-        console.log(req);
-
         res.json({ msg: 'User deleted' });
     } catch (err) {
         console.error(err.message);
         res.status(500).send('Server Error');
     }
 });
+
+// @route   PUT api/profile/experience
+// @desc    Add profile experience
+// @access  Private
+router.put(
+    '/experience',
+    [
+        auth,
+        [
+            check('title', 'Title is required')
+                .not()
+                .isEmpty(),
+            check('company', 'Company is required')
+                .not()
+                .isEmpty(),
+            check('from', 'From date is required')
+                .not()
+                .isEmpty(),
+        ]
+    ],
+    async (req, res) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() });
+        }
+
+        // Destructuring to get the body data
+        const {
+            title,
+            company,
+            location,
+            from,
+            to,
+            current,
+            description
+        } = req.body;
+
+        const newExp = {
+            // title is the same as using title: title
+            // Create an object with the data the user submits
+            title,
+            company,
+            location,
+            from,
+            to,
+            current,
+            description
+        }
+
+        try {
+            const profile = await Profile.findOne({ user: req.user.id })
+
+            profile.experience.unshift(newExp);
+
+            await profile.save();
+
+            res.json(profile);
+        } catch (err) {
+            console.error(err.message);
+            res.status(500).send('Server Error');
+        }
+    }
+);
 
 module.exports = router;
