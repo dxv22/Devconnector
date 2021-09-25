52be52b601f606e0873794345389db8a8e733918
Daxxon Vong <Daxxonvong@yahoo.co.nz>
2021-09-25 23:08:16 +1200

    Added profile top and profile about section

--- client/src/components/profile/Profile.js
+++ client/src/components/profile/Profile.js
@@ -3,6 +3,8 @@ import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import Spinner from '../layout/Spinner';
 import { getProfileByID } from '../../actions/profile';
+import ProfileTop from './ProfileTop';
+import ProfileAbout from './ProfileAbout';
 import { Link } from 'react-router-dom';
 
 const Profile = ({ 
@@ -17,7 +19,14 @@ const Profile = ({
   return <Fragment>
     {profile === null || loading ? <Spinner /> : <Fragment>
       <Link to="/profiles" className="btn btn-light">Back To Profiles</Link>
-      { auth.isAuthenticated && auth.loading === false && auth.user._id === profile.user._id && <Link to="/edit-profile" className="btn btn-dark">Edit Profile</Link>}
+      { auth.isAuthenticated && auth.loading === false && auth.user._id === profile.user._id && (
+      <Link to="/edit-profile" className="btn btn-dark">
+        Edit Profile
+        </Link>)}
+        <div class="profile-grid my-1">
+          <ProfileTop profile={profile} />
+          <ProfileAbout profile={profile} />
+        </div>
       </Fragment>}
   </Fragment>
 };
