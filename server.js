8f083f55b01c7f8f64ba41c2195999766a4c960b
Daxxon <daxxonvong@yahoo.co.nz>
2021-10-05 21:40:37 +1300

    Revert "prepare for deployment"

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const connectDB = require('./config/db');
-const path = require('path');
+
 const app = express();
 
 // Connect Database
@@ -9,22 +9,13 @@ connectDB();
 // Init Middleware
 app.use(express.json({ extended: false}))
 
+app.get('/', (req, res) => res.send('API Running'));
+
 // Define Routes
 app.use('/api/users', require('./routes/api/users'));
 app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/profile', require('./routes/api/profile'));
 app.use('/api/posts', require('./routes/api/posts'));
-
-// Serve static assets in production
-if(process.env.NODE_ENV === 'production') {
-  // Set static folder app
-  app.use(express.static('client/build'));
-}
-
-// Create a route to serve the file
-app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-});
 
 const PORT = process.env.PORT || 5000;
 
